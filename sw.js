02ac7d6362b2f3d3088f352d265b6c34a38c9743
Carll91 <xpslana00@gmail.com>
2026-07-09 20:50:56 +0200

    Update service worker to version 7

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* ============== POPQUIZ SERVICE WORKER ============== */
-const CACHE = "popquiz-v6";
+const CACHE = "popquiz-v7";
 
 // Soubory, které se uloží do cache při instalaci
 const ASSETS = [
